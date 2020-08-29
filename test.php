047e8c4ae569d3d15e1cfe1d84df9b717688e128
soif <wxopwx@gmail.com>
2020-08-29 02:35:10 +0200

    Faster UbusListStations

--- test.php
+++ test.php
@@ -2,48 +2,56 @@
 <?php
 
 /*
-# https://openwrt.org/docs/techref/ubus
+
+For all Ubus Calls, refer to:
+https://openwrt.org/docs/techref/ubus
+
 */
 
-
-$i=0;
-$cfg['hosts'][$i]['host']="OpenWRT IP ADDRESS";
-$cfg['hosts'][$i]['user']="root";
-$cfg['hosts'][$i]['pass']="PASSWORD";
-
+// Fill this 
+$cfg['host']="OpenWRT IP ADDRESS";
+$cfg['user']="root";
+$cfg['pass']="PASSWORD";
 
 $debug=false;
+
+
 
 // --------------------------------------------------------------------------
 
 require_once(dirname(__FILE__)."/src/owa.php");
 
-$host=$cfg['hosts'][0];
+$owa=new OpenWrtApi("http://".$cfg['host'] , $debug);
+$owa->UbusLogin($cfg['user'], $cfg['pass']);
 
-$owa=new OpenWrtApi("http://".$host['host'] , $debug);
-$owa->UbusLogin($host['user'], $host['pass']);
 
-TestCallUbus('system','board');
-TestCallUbus('system','info');
+echo "<pre>";
 
-if($devices=TestCallUbus('luci-rpc','getWirelessDevices')){
+TestUbusCall('system','board');
+TestUbusCall('system','info');
+
+// grab all interfaces
+if($devices=TestUbusCall('luci-rpc','getWirelessDevices')){
 	foreach($devices as $dev => $info){
 		foreach($info['interfaces'] as $interface ){
-			$ifname=$interface['ifname'];
-			$stations=$owa->CallUbus('iwinfo','assoclist',array('device'=>$ifname));
-			foreach($stations['results'] as $s){
-				echo "$ifname	{$s['mac']}\n";
-
-			}
+			$ifs[]=$interface['ifname'];
 		}
+	}
+
+	// grab all stations / interface
+	if(is_array($ifs)){
+		echo "--- STATIONS ----------------------------------------------------------------\n";
+		$r=$owa->UbusListStations($ifs);
+		print_r($r);		
 	}
 }
 
+
 // -------------------------------------------------------------------
-function TestCallUbus($path,$meth, $arg=array()){
+function TestUbusCall($path,$meth, $arg=array()){
 	global $owa;
-	echo "## $path - $meth ########################################\n";
-	$r=$owa->CallUbus($path,$meth, $arg);
+	echo str_pad("## $path - $meth ",80,'#')."\n";
+	$r=$owa->UbusCall($path,$meth, $arg);
 	if($r){
 		print_r($r);
 		return $r;
